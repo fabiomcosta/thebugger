3198abdce8da6e3740124949b68fb2dc1375ca7e
Narek Hakobyan <h_narek2007@mail.ru>
2018-03-02 11:43:34 +0400

    remove debugger
    remove debugger; to be able to debug the serverless app
    Problem is the debugger is stopping in each request

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const inspector = require('inspector');
 function thebugger({port, host} = {}) {
   const {debugOptions} = process.binding('config');
   inspector.open(port || debugOptions.port, host || debugOptions.host, true);
-  debugger;
 }
 
 module.exports = thebugger;
